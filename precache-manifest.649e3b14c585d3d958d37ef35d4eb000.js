2c1b42bf972f045d64b7bcbd67e8573dfcbf5c43
Jordan Lesich <jordan.lesich@gmail.com>
2020-08-12 10:40:10 -0400

    Updates

---
+++ precache-manifest.649e3b14c585d3d958d37ef35d4eb000.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29360880054c633911bc0a6939d44727",
+    "url": "/steppinwolf/index.html"
+  },
+  {
+    "revision": "e4a4ff33b229e84445ea",
+    "url": "/steppinwolf/static/css/main.677d7612.chunk.css"
+  },
+  {
+    "revision": "54f38da2fcb9709f7bd2",
+    "url": "/steppinwolf/static/js/2.8cbc62d1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/steppinwolf/static/js/2.8cbc62d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4a4ff33b229e84445ea",
+    "url": "/steppinwolf/static/js/main.e4ee9b84.chunk.js"
+  },
+  {
+    "revision": "4519b00905b3cd02f028",
+    "url": "/steppinwolf/static/js/runtime-main.fbd09f04.js"
+  }
+]);
